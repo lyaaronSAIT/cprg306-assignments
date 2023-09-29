de629d36bc1d9b0c4035dc429869c0acd4ae110e
lyaaronSAIT <130510117+lyaaronSAIT@users.noreply.github.com>
2023-09-28 21:35:19 -0600

    Updated page.js, created week2 and StudentInfo.js

---
+++ app/StudentInfo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const StudentInfo = () => {
+    return (
+        <div>
+            <h2>Name: Aaron Ly</h2>
+            <h2>Course section: CPRG 306 D</h2>
+            <a href="https://github.com/lyaaronSAIT/cprg306-assignments">Aaron Github</a>
+        </div>
+    );
+};
+
+export default StudentInfo;
---
+++ app/week2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "../StudentInfo";
+
+export default function Shoppinglist() {
+    return (
+        <main>
+            <h1>My Shopping List</h1>
+            <StudentInfo></StudentInfo>
+        </main>
+    );
+}
