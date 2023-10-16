0b18a1cb768d360cc2004ec63877428a187d7d94
lyaaronSAIT <130510117+lyaaronSAIT@users.noreply.github.com>
2023-10-16 17:22:08 -0600

    render the component, added week4 link

--- app/week4/new-items.js
+++ app/week4/new-items.js
@@ -26,5 +26,68 @@ export default function NewItem() {
 
     };
 
-    
+    const nameChange = (event) => {
+        setName(event.target.value);
+    };
+
+    const quantityChange = (event) => {
+        setQuantity(event.target.value);
+    };
+
+    const categoryChange = (event) => {
+        setCategory(event.target.value);
+    };
+
+    return (
+        <main>
+                <div>
+                <h1>Add New Item</h1>
+                    <form onSubmit={handleSubmit}>
+                        <label>
+                            <span>Item Name </span>
+                            <input 
+                                required onChange={nameChange}
+                                value={name}
+                            ></input>
+                        </label>
+
+                        <label>
+                            <span> Quantity </span>
+                            <select
+                                required onChange={quantityChange}
+                                value={quantity}
+                                type="number"
+                                min={1}
+                                max={99}
+                            ></select>
+                        </label>
+
+                        <label>
+                            <span> Category </span>
+                            <select 
+                                required onChange={categoryChange}
+                                value={category}>
+
+                                <option value="Produce">Produce</option>
+                                <option value="Dairy">Dairy</option>
+                                <option value="Bakery">Bakery</option>
+                                <option value="Meat">Meat</option>
+                                <option value="Frozen Foods">Frozen Foods</option>
+                                <option value="Canned Goods">Canned Goods</option>
+                                <option value="Dry Goods">Dry Goods</option>
+                                <option value="Beverages">Beverages</option>
+                                <option value="Snacks">Snacks</option>
+                                <option value="Household">Household</option>
+                                <option value="Other">Other</option>
+                            </select>
+                        </label>
+
+                        <button type="submit">
+                             Submit Item 
+                        </button>
+                    </form>
+                </div>
+            
+        </main>
+    );
 }
---
+++ app/week4/page.js
@@ -0,0 +1,9 @@
+import NewItem from "./new-items";
+
+export default function Page(){
+    return (
+        <main>
+            <NewItem/>
+        </main>
+    );
+}
