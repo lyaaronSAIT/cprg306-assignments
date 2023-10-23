f64be788aa35c8cea682b094a770fac583af8d2e
lyaaronSAIT <130510117+lyaaronSAIT@users.noreply.github.com>
2023-10-22 20:18:06 -0600

    added Tailwind CSS

--- app/week5/item-list.js
+++ app/week5/item-list.js
@@ -14,12 +14,12 @@ export default function itemList() {
         <div className="flex flex-col items-center justify-center">
             <div className="space-y-4"> Sort by:
                 <button
-                    className={`px-2 py-1 ${sortBy === 'name' ? 'bg-blue-500' : 'bg-blue-200 text-blue-800'}`}
+                    className={`px-2 py-1 ${sortBy === 'name' ? 'bg-violet-700' : 'bg-violet-200 text-blue-800'}`}
                     onClick={() => setSortBy("name")}> Sort by Name
                 </button>
 
                 <button
-                    className={`px-2 py-1 ${sortBy === 'category' ? 'bg-blue-500' : 'bg-blue-200 text-blue-800'}`}
+                    className={`px-2 py-1 ${sortBy === 'category' ? 'bg-violet-500' : 'bg-violet-200 text-blue-800'}`}
                     onClick={() => setSortBy("category")}>Sort by Category
                 </button>
                 
--- app/week5/item.js
+++ app/week5/item.js
@@ -1,10 +1,12 @@
 export default function Item({name, quantity, category}) {
     return(
         <div className="space-y-10">
-            <div>
-                <p className="flex justify-center font-bold text-violet-700">Item name: {name}</p>
-                <p className="flex justify-center">Buy {quantity} of them in the {category} aisle.</p>
-            </div>
+            <ul>
+                <li className="p-2 m-4 max-w-sm bg-slate-900">
+                    <p className="flex justify-center font-bold text-violet-700">Item name: {name}</p>
+                    <p className="flex justify-center">Buy {quantity} of them in the {category} aisle.</p>
+                </li>
+            </ul>
         </div>
     );
 }
