8ec9bbdcb717181137dfdc623e63ba8ff698d533
lyaaronSAIT <130510117+lyaaronSAIT@users.noreply.github.com>
2023-10-22 19:33:46 -0600

    week5 first commit
    creating the week5 files

--- app/page.js
+++ app/page.js
@@ -15,6 +15,9 @@ export default function Home() {
       <p>
         <Link href='/week4'>Week 4</Link>
       </p>
+      <p>
+        <Link href='/week5'>Week 5</Link>
+      </p>
     </main>
   );
 }
